e0680871d1b3d68ece6c3c485f521d3dc7dfead9
Valla Hallama <valla.hallama@gmail.com>
2022-04-29 13:18:02 +0300

    add printing message

--- src/asm/write_file.c
+++ src/asm/write_file.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   write_file.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vhallama <vhallama@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: vhallama <vhallama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 16:35:13 by vhallama          #+#    #+#             */
-/*   Updated: 2022/04/27 14:56:53 by vhallama         ###   ########.fr       */
+/*   Updated: 2022/04/29 13:17:28 by vhallama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void	write_file(t_data *data)
 	data->target_fd = open(data->filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (data->target_fd == -1)
 		error_exit("Error: cannot open output file");
+	ft_printf("Writing output to %s\n", data->filename);
 	write_header(data);
 	if (close(data->target_fd) == -1)
 		error_exit("Error: cannot close output file");
