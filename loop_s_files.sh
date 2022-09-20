c8f732b30a470c2e80661f65b4a6a66bea9e335a
Riki Kyttälä <rkyttala@student.hive.fi>
2022-09-20 20:06:27 +0300

    cleaned up the repo

--- loop_s_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in resources/champs/*
-do
-    echo "$file"
-    [ -f "$file" ] || continue
-    ./asm "$file" > /dev/null
-done
--- tests/loop_s_files.sh
+++ tests/loop_s_files.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+cp ../asm .
 for file in ../resources/champs/*
 do
     echo "$file"
     [ -f "$file" ] || continue
-    ../asm "$file" > /dev/null
+    ./asm "$file" > /dev/null
 done
+rm -f asm
+
