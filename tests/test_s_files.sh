55ec671b7b69fc5e3fe67ab7bb7ccd10ecf36ea5
Valla Hallama <valla.hallama@gmail.com>
2022-09-25 18:43:52 +0300

    rename test script

--- tests/test_s_files.sh
+++ tests/test_s_files.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-cp ../asm .
-for file in ../resources/champs/*
+for file in resources/champs/*
 do
     echo "$file"
     [ -f "$file" ] || continue
     ./asm "$file" > /dev/null
 done
-rm -f asm
-
