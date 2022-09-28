7613009065d731695a3037059abd81a6ba7a695e
Riki Kyttälä <rkyttala@student.hive.fi>
2022-09-28 23:01:53 +0300

    normed

--- src/corewar/sort_champs.c
+++ src/corewar/sort_champs.c
@@ -6,51 +6,51 @@
 /*   By: rikikyttala <rikikyttala@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 11:42:23 by rikikyttala       #+#    #+#             */
-/*   Updated: 2022/09/28 22:28:58 by rikikyttala      ###   ########.fr       */
+/*   Updated: 2022/09/28 23:01:13 by rikikyttala      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-static void add_unordered_champs(t_champ *champs, t_flags *flags)
+static void	add_unordered_champs(t_champ *champs, t_flags *flags)
 {
-    int     p;
-    int     i;
-    t_champ unsorted[MAX_PLAYERS];
+	int		p;
+	int		i;
+	t_champ	unsorted[MAX_PLAYERS];
 
-    p = -1;
-    while (++p < flags->champ_count)
-    {
-        if (champs[p].playernbr < 0)
-            unsorted[ft_abs(champs[p].playernbr) - 1] = champs[p];
-    }
-    p = 0;
-    i = 0;
-    while (++p <= flags->champ_count)
-    {
-        if (flags->used_nbrs[p - 1] == 0)
-        {
-            champs[p - 1] = unsorted[i];
-            champs[p - 1].playernbr = p;
-            i++;
-        }
-    }
+	p = -1;
+	while (++p < flags->champ_count)
+	{
+		if (champs[p].playernbr < 0)
+			unsorted[ft_abs(champs[p].playernbr) - 1] = champs[p];
+	}
+	p = 0;
+	i = 0;
+	while (++p <= flags->champ_count)
+	{
+		if (flags->used_nbrs[p - 1] == 0)
+		{
+			champs[p - 1] = unsorted[i];
+			champs[p - 1].playernbr = p;
+			i++;
+		}
+	}
 }
 
 static void	mark_unordered_champs(t_champ *champs, t_flags *flags)
 {
 	int	i;
-	int unordered_no;
+	int	unordered_no;
 
 	i = -1;
 	unordered_no = -1;
 	while (++i < flags->champ_count)
 	{
 		if (champs[i].playernbr < 1)
-        {
+		{
 			champs[i].playernbr = unordered_no;
-		    unordered_no--;
-        }
+			unordered_no--;
+		}
 	}
 }
 
@@ -63,14 +63,14 @@ static void	mark_unordered_champs(t_champ *champs, t_flags *flags)
 void	sort_champs(t_champ *champs, t_flags *flags)
 {
 	int		p;
-    int     curr;
+	int		curr;
 	t_champ	tmp;
 
-    mark_unordered_champs(champs, flags);
+	mark_unordered_champs(champs, flags);
 	p = 1;
 	while (p <= flags->champ_count)
 	{
-        curr = champs[p - 1].playernbr;
+		curr = champs[p - 1].playernbr;
 		if (curr > 0)
 		{
 			if (curr > flags->champ_count)
@@ -83,7 +83,7 @@ void	sort_champs(t_champ *champs, t_flags *flags)
 				continue ;
 			}
 		}
-        p++;
+		p++;
 	}
-    add_unordered_champs(champs, flags);
+	add_unordered_champs(champs, flags);
 }
--- test.s
+++
@@ -1,6 +0,0 @@
-.name "testi"
-.comment "testo"
-
-thang:
-    ld      %23041923891823, r10
-    zjmp    %0
